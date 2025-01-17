ecff24c101c155fc95308ca4d56a8e39e138a035
limingshuang <limingshuang@zuzuche.com>
2025-01-17 16:57:23 +0800

    refactor: update install.sh - install commonds by git alias
    Signed-off-by: limingshuang <limingshuang@zuzuche.com>

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-install -m 755 git-* /usr/local/bin
+if test -f git-paging-alias.txt;then 
+    git config --global --add include.path $(readlink -f git-paging-alias.txt)
+else
+    install -m 755 git-* /usr/local/bin
+fi
